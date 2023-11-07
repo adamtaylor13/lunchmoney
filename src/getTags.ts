7add79c4facb6ee572d0118aaee7de8c95f8cb5f
Adam Taylor <adamtaylor0795@gmail.com>
2023-11-07 15:26:29 -0600

    Update getTags to use DEFAULT_HEADERS

--- src/getTags.ts
+++ src/getTags.ts
@@ -1,11 +1,9 @@
-import { LM_API_TOKEN, TAGS_API } from "./config.ts";
+import { DEFAULT_HEADERS, TAGS_API } from "./config.ts";
 import { Tag } from "./types.ts";
 
 export async function getTags(): Promise<Tag[]> {
   const response = await fetch(TAGS_API, {
-    headers: {
-      Authorization: `Bearer ${LM_API_TOKEN}`,
-    },
+    headers: { ...DEFAULT_HEADERS },
   });
   return await response.json();
 }
