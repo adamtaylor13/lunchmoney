3bdc4990d32f12883a17adef592ae3d883b7e71b
Adam Taylor <adamtaylor0795@gmail.com>
2023-11-07 15:30:29 -0600

    Update tag test request

--- src/getTags.test.ts
+++ src/getTags.test.ts
@@ -1,7 +1,22 @@
-import { assertEquals } from "https://deno.land/std@0.186.0/testing/asserts.ts";
+import { assertEquals, denock } from "./test_deps.ts";
 import { getTags } from "./getTags.ts";
 
 Deno.test(async function getAllTags() {
-  const tags = await getTags();
-  assertEquals(tags.length > 0, true);
+  const tagsApi = denock({
+    method: "GET",
+    protocol: "https",
+    host: "dev.lunchmoney.app",
+    path: "/v1/tags",
+    replyStatus: 200,
+    responseBody: [
+      {
+        id: 123,
+        name: "My Tag",
+        description: "This tag does stuff",
+        archived: false,
+      },
+    ],
+  });
+  await getTags();
+  assertEquals(tagsApi.called(), 1);
 });
